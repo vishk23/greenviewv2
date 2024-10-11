f557d7fa977a9808ec004199e187613b3dfe4f18
Vishnu Kchitti <VKchitti@its.jnj.com>
2024-10-11 11:19:38 -0400

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addWebpackAlias } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addWebpackAlias({
+    '@features': path.resolve(__dirname, 'src/features'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@contexts': path.resolve(__dirname, 'src/contexts'),
+    '@public': path.resolve(__dirname, 'public')
+  })
+);
---
+++ src/features/Calendar/CalendarPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyCalendar from './Calendar';
+
+const CalendarPage: React.FC = () => {
+  return (
+    <div>
+      <MyCalendar />
+    </div>
+  );
+};
+
+export default CalendarPage;
