eda09dd0bef3256e07c1bcca3bc2d6f83dc532b2
Wes1ey Ch0i <114440275+Wes1eyCh0i@users.noreply.github.com>
2024-11-07 12:29:47 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Calendar from "./pages/Calendar";
 import Score from "./pages/Score";
 import Main from "./pages/Main";
@@ -10,7 +9,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/home" element={<Home />} />
         <Route path="/score" element={<Score />} />
