47f1090ee62ddc0705a5c80801ba351c02bc6225
Ana <anasof@bu.edu>
2024-11-09 10:43:26 -0500

    quick fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Calendar from "./pages/Calendar";
 import Score from "./pages/Score";
-import Main from "./pages/Main";
+import Main from "./pages/Home";
 import Summary from "./pages/Summary";
 import Leaderboard from "./features/Leaderboard/Leaderboard";
 
@@ -11,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/calendar" element={<Calendar />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Main />} />
         <Route path="/score" element={<Score />} />
         <Route path="/summary" element={<Summary />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
