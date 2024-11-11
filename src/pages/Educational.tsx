b86cb8f2affe65c452f34b48bd5e4a114b6b8081
Wes1ey Ch0i <114440275+Wes1eyCh0i@users.noreply.github.com>
2024-11-11 13:09:53 -0500

    Update Educational.tsx

--- src/pages/Educational.tsx
+++ src/pages/Educational.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Educational from "@features/Educational/Educational";
-import Map from "@features/Map/Map";
+// import Map from "@features/Map/Map";
 // import { features } from "process";
 
 const App: React.FC = () => {
